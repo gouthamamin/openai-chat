e0e36d217f67f91c5e9e8a294a758a9ed7016f23
Goutham Amin <gouthamamin@gmail.com>
2025-05-24 15:24:06 +0530

    add main layout with input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BsSend } from "react-icons/bs";
+
 const App = () => {
   return (
     <div className="w-full h-full bg-primaryBackground flex">
@@ -18,8 +20,23 @@ const App = () => {
       </div>
 
       {/* main content */}
-      <div className="w-[80%] h-full bg-themeGray-44">
+      <div className="w-[80%] h-full bg-themeGray-44 flex flex-col">
+        <div className="w-full h-[10%] flex justify-center items-center bg-themeGray-40 text-white">
+          Welcome to openai chat
+        </div>
+        <div className="w-full h-[75%] bg-themeGray-44">
 
+        </div>
+        <div className="w-full h-[15%] flex flex-col gap-2 p-4 justify-center items-center bg-themeGray-40 ">
+          <div className="inline-flex justify-center items-center text-white text-md gap-2 border border-white rounded-md px-4 py-1 cursor-pointer">
+            <img src="./images/regenerate-icon.png" alt="regenerate-icon" className="w-4 h-4" />
+            <p>Regenarate response</p>
+          </div>
+          <div className="w-[40%] flex justify-center items-center bg-themeGray-8E rounded-md">
+            <input type="text" placeholder="Ask anything" className="h-10 flex-1 text-white px-4 py-2 bg-transparent outline-none focus:outline-none" />
+            <BsSend className="w-10 h-10 text-white cursor-pointer p-2" />
+          </div>
+        </div>
       </div>
     </div>
   );
